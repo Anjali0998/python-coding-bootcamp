5886170d13c28095dc03c6d4ea35208f907d9e69
anjalitoliya <anjalitoliya09@gmail.com>
2021-02-17 14:18:07 +0530

    todo assignment

---
+++ session4/Even,odd.py
@@ -0,0 +1,5 @@
+for i in range(1,11): # runs from (1 to 10)
+    if i%2==0:
+        print(i, "is even")
+    else:
+        print(i, "is odd")
---
+++ session4/lamp-on-or-not.py
@@ -0,0 +1,7 @@
+print("is lamp is turned on? (true/false)")
+LampTurnedOn = "true"
+
+while LampTurnedOn == "true":
+    LampTurnedOn = input("please continue reading book. \n is lamp is turned on? (true/false)")
+
+print("lamp is turned offssss. Go to sleep!")
